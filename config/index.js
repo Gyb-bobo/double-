d0c170f18e8ce0168e735f9e00fb02d7bcf4257a
Gyb-bobo <114222327+Gyb-bobo@users.noreply.github.com>
2022-09-27 15:49:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,9 +62,9 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id:"oj-pB6UaF_Cnei4r0VPytQO_VlFE",
+      id:"oj-pB6YsXWfbEf84JGNU3_8CL6y0E",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "SPuKrLLxlE0N8xQIVddoZyvfZoVeX-s6RAe5UHfDyXo",
+      useTemplateId: "eEkywYeTlmWiCXM4KjsrSVICoJWxcrAj-EonpDe1JNo",
       // 所在省份
       province: "天津",
       // 所在城市
